94bdf55f08e4e700e3ad1481cfed2d7fef8e6539
Noppadon Homjan <93645427+Puddingz112@users.noreply.github.com>
2021-12-26 15:52:43 +0700

    first commit

---
+++ default/assets/script.vue
@@ -0,0 +1,17 @@
+
+  
+    var name = new Vue({
+    el: '#name',
+    data: {
+        name: 'Noppadon Homjan',
+        names: 'Sirimongkol Tharin',
+        names2: 'Chakris kettusuja',
+        names3: 'Santiphap Manowong',
+        names4: 'Suphakit Palanan',
+        type: 'Student, MJU',
+        temp1: 'HTML',
+        temp2: 'CSS',
+        temp3: 'BOOTSTRAP',
+        temp4: 'VUE'
+    },
+    })
